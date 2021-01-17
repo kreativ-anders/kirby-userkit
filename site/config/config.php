736f6bc4d7121ad680497bf18347630cbbc4a9f6
Manuel Steinberg <github@manuel-steinberg.de>
2021-01-17 11:30:22 +0100

    add virtual logout page

--- site/config/config.php
+++ site/config/config.php
@@ -8,8 +8,22 @@
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
-    'debug' => true,
+  'debug' => true,
 	'panel' =>[
 		'install' => true
-	  ]
+	],
+	'routes' => [
+    [
+      'pattern' => 'logout',
+      'action'  => function() {
+
+        if ($user = kirby()->user()) {
+          $user->logout();
+        }
+
+        go('login');
+
+      }
+    ]
+  ]
 ];
