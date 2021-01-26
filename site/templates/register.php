6cf4abdf689c4f0114c2409ee06023860e734686
Manuel Steinberg <github@manuel-steinberg.de>
2021-01-26 14:19:16 +0100

    update register controller
    - remove comment
    - add notification snippet
    - format HTML

--- site/templates/register.php
+++ site/templates/register.php
@@ -1,45 +1,30 @@
-<?php
-/*
-  Templates render the content of your pages.
-
-  They contain the markup together with some control structures
-  like loops or if-statements. The `$page` variable always
-  refers to the currently active page.
-
-  To fetch the content from each field we call the field name as a
-  method on the `$page` object, e.g. `$page->title()`.
-
-  This default template must not be removed. It is used whenever Kirby
-  cannot find a template with the name of the content file.
-
-  Snippets like the header and footer contain markup used in
-  multiple templates. They also help to keep templates clean.
-
-  More about templates: https://getkirby.com/docs/guide/templates/basics
-*/
-?>
 <?php snippet('header') ?>
-
-<h1><?= $page->title()->html() ?></h1>
-
-<?php if($error): ?>
-<div class="alert"><?= $page->alert()->html() ?></div>
-<?php endif ?>
+<?php snippet('intro') ?>
+<?php snippet('notification') ?>
 
 <form method="post" action="<?= $page->url() ?>">
-  <div>
-    <label for="email"><?= $page->email()->html() ?></label>
-    <input type="email" id="email" name="email" value="<?= esc(get('email')) ?>" required>
-    <p><i style="color: grey"><?= $page->email_info()->html() ?></i></p>
-  </div>
-  <div>
-    <label for="password"><?= $page->password()->html() ?></label>
-    <input type="password" id="password" name="password" value="<?= esc(get('password')) ?>" required>
-    <p><i style="color: grey"><?= $page->password_info()->html() ?></i></p>
-  </div>
-  <div>
-    <input type="submit" name="register" value="<?= $page->button()->html() ?>">
-  </div>
+  <fieldset>
+    <legend><?= $page->title()->html() ?></legend>
+
+    <section>
+      <label for="email"><?= $page->email()->html() ?></label>
+      <input type="email" id="email" name="email" value="<?= esc(get('email')) ?>" autocomplete="email" autofocus
+        required>
+      <p><i><?= $page->email_info()->html() ?></i></p>
+    </section>
+
+    <section>
+      <label for="password"><?= $page->password()->html() ?></label>
+      <input type="password" id="password" name="password" value="<?= esc(get('password')) ?>"
+        autocomplete="new-password" required>
+      <p><i><?= $page->password_info()->html() ?></i></p>
+    </section>
+
+    <section>
+      <input type="submit" name="register" value="<?= $page->button()->html() ?>">
+    </section>
+
+  </fieldset>
 </form>
 
 <?php snippet('footer') ?>
