6a012f57692a55abf8b71706e077a5bd070ad521
Manuel Steinberg <github@manuel-steinberg.de>
2021-01-26 14:30:15 +0100

    format HTML

--- site/templates/login.php
+++ site/templates/login.php
@@ -3,17 +3,27 @@
 <?php snippet('notification') ?>
 
 <form method="post" action="<?= $page->url() ?>">
-  <div>
-    <label for="email"><?= $page->email()->html() ?></label>
-    <input type="email" id="email" name="email" value="<?= esc(get('email')) ?>" required>
-  </div>
-  <div>
-    <label for="password"><?= $page->password()->html() ?></label>
-    <input type="password" id="password" name="password" value="<?= esc(get('password')) ?>" required>
-  </div>
-  <div>
-    <input type="submit" name="login" value="<?= $page->button()->html() ?>">
-  </div>
+  <fieldset>
+    <legend><?= $page->title()->html() ?></legend>
+
+    <section>
+      <label for="email"><?= $page->email()->html() ?></label>
+      <input type="email" id="email" name="email" value="<?= esc(get('email')) ?>" autocomplete="email" autofocus
+        required>
+    </section>
+
+    <section>
+      <label for="password"><?= $page->password()->html() ?></label>
+      <input type="password" id="password" name="password" value="<?= esc(get('password')) ?>"
+        autocomplete="current-password" required>
+      <p><i><?= $page->password_info()->html() ?></i></p>
+    </section>
+
+    <section>
+      <input type="submit" name="login" value="<?= $page->button()->html() ?>">
+    </section>
+
+  </fieldset>
 </form>
 
 <?php snippet('footer') ?>
